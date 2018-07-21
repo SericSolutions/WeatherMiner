0650465a6aead6398ab12910ad61a3f0193b5c6f
asad <Seric.solutions@gmail.com>
2018-07-21 15:17:04 +0500

    first commit

---
+++ allParams.js
@@ -0,0 +1,18 @@
+const weatherUndergroundParams = [
+  "alerts",	//Returns the short name description, expiration time and a long text description of a severe alert — if one has been issued for the searched upon location.
+  "almanac",	//Historical average temperature for today
+  "astronomy",	//Returns the moon phase, sunrise and sunset times.
+  "conditions",	//Returns the current temperature, weather condition, humidity, wind, 'feels like' temperature, barometric pressure, and visibility.
+  "currenthurricane",	//Returns the current position, forecast, and track of all current hurricanes.
+  "forecast",	//Returns a summary of the weather for the next 3 days. This includes high and low temperatures, a string text forecast and the conditions.
+  "forecast10day",	//Returns a summary of the weather for the next 10 days. This includes high and low temperatures, a string text forecast and the conditions.
+  "geolookup",	//Returns the the city name, zip code / postal code, latitude-longitude coordinates and nearby personal weather stations.
+  "history",	//history_YYYYMMDD returns a summary of the observed weather for the specified date.
+  "hourly",	//Returns an hourly forecast for the next 36 hours immediately following the API request.
+  "hourly10day",	//Returns an hourly forecast for the next 10 days
+  "planner",	//planner_MMDDMMDD returns a weather summary based on historical information between the specified dates (30 days max).
+  "rawtide",	//Raw Tidal information for graphs
+  "tide",	//Tidal information
+  "webcams",	//Returns locations of nearby Personal Weather Stations and URL's for images from their web cams.
+  "yesterday",	//Returns a summary of the observed weather history for yesterday.
+]
